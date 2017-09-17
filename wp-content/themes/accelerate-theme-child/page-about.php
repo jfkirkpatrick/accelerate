e280cf48379884274bf342f8da29acc214d92b6b
jfkirkpatrick <jfkirk@ix.netcom.com>
2017-09-17 12:54:11 -0700

    Final About Page

--- wp-content/themes/accelerate-theme-child/page-about.php
+++ wp-content/themes/accelerate-theme-child/page-about.php
@@ -16,12 +16,11 @@ get_header();
 
 <div id="primary" class="site-content">
     <div class="main-content" role="main">
-        <section>
-            <div class="about-page-sections">              
+        <div class="about-page-sections">             <section>        
                 <p class="our-services">Our Services</p>
                 <p class="our-services-text">We take pride in our clients and the content we create for them.<br>
                     Here is a brief overview of our offered services</p> 
-
+            </section>
                 <?php query_posts('post_type=our_services&orderby=title&order=ASC'); ?>
                 <?php
                 $cntr = 0;
@@ -30,28 +29,50 @@ get_header();
                     $services_text = get_field('services_text');
                     $services_image = get_field('services_image');
                     $size = "medium";
+                    
                     ?>
 
-                    <article class="content-strategy-main-left clearfix">     
-                        
-                           <figure class="content-strategy-image-left">
+                    <article class="content-strategy-main clearfix"> 
+                      <?php $cntr = $cntr + 1;  ?>  
+                        <?php if ($cntr % 2 == 0) { ?>  <!-- Even  -->
+                           <figure class="content-strategy-image-right">
                             <?php
                             if ($services_image) {
                                 echo wp_get_attachment_image($services_image, $size);
                             }
                             ?>
                             </figure>
-                            <div class="content-strategy-content">
+                            <div class="content-strategy-content-left">
                                 <p class="content-strategy-title"><?php echo $services_title; ?> </p>
                                 <p class="content-strategy-text"><?php echo $services_text; ?> </p>
                             </div>
-            
+                            <?php
+                        } else { ?> <!-- Odd  -->
+                            <figure class="content-strategy-image-left">
+                            <?php
+                            if ($services_image) {
+                                echo wp_get_attachment_image($services_image, $size);
+                            }
+                            ?>
+                            </figure>
+                            <div class="content-strategy-content-right">
+                                <p class="content-strategy-title"><?php echo $services_title; ?> </p>
+                                <p class="content-strategy-text"><?php echo $services_text; ?> </p>
+                            </div>
+                            <?php } ?>
                     </article>
                     <br>
                 <?php endwhile; // end of the loop.    ?>
                 <?php wp_reset_query(); ?>
-            </div><!-- End of About Page Div -->
-        </section><!-- End of About Page Section -->
+                <div class="call-to-action">
+                   <div class="align-center vertical-middle">
+                       <span class="call-to-action-text">Interested in working for us?</span>
+                        <a class="call-to-action-button" href="<?php echo site_url(); ?>/contact-us">Contact Us</a>
+                    </div>
+                </div>
+              
+        </div><!-- End of About Page Div -->
+        
     </div><!-- End of Main Content Class  -->
 </div><!-- End of Primary Class  -->
 
